dfd852c10273b51f8cbec0fa0207ddf335da81db
Peng Zhou <zhoupengcv@sjtu.edu.cn>
2018-07-23 22:22:06 +0800

    ./run.sh e2e_mask_rcnn_R-101-FPN_2x debug 1

--- run.sh
+++ run.sh
@@ -36,6 +36,7 @@ fi
 
 export PYTHONPATH=/home/Peterou/Desktop/usr/code/caffe2/pytorch/build/libinstall/usr/local:/home/Peterou/Desktop/usr/code/caffe2/pytorch/build/libinstall/usr/local/lib/python2.7/site-packages:/home/Peterou/Desktop/usr/code/caffe2/detectron:$PYTHONPATH
 export LD_LIBRARY_PATH=/home/Peterou/Desktop/usr/code/caffe2/pytorch/build/libinstall/usr/local/lib:/usr/local/cudnn_v6/lib64:/usr/local/cuda-8.0/lib64
+export CUDA_VISIBLE_DEVICES=1
 
 source ~/anaconda3/bin/activate caffe2
 
@@ -58,6 +59,36 @@ then
     --image-ext jpg \
     --wts models/model_final.pkl \
     demo
+
+    delete_debug_string $file $line "$debug_str"
+
+elif [ $1 = e2e_mask_rcnn_R-101-FPN_2x ]
+then
+    # ./run.sh e2e_mask_rcnn_R-101-FPN_2x debug 1
+    # weights: https://s3-us-west-2.amazonaws.com/detectron/35861858/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml.02_32_51.SgT4y1cO/output/train/coco_2014_train:coco_2014_valminusminival/generalized_rcnn/model_final.pkl
+    debug=$2
+    gpus=$3
+    file="tools/test_net.py"
+    line=24
+    insert_debug_string $file $line "$debug_str" "$debug"
+
+    if [ $gpus > 1 ]
+    then
+
+        python "$file" \
+        --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
+        --multi-gpu-testing \
+        TEST.WEIGHTS  ./models/model_final.pkl.1 \
+        TRAIN.WEIGHTS ./models/model_final.pkl.1 \
+        NUM_GPUS $"$gpus"
+    else
+        python "$file" \
+            --cfg configs/12_2017_baselines/e2e_mask_rcnn_R-101-FPN_2x.yaml \
+            TEST.WEIGHTS  ./models/model_final.pkl.1 \
+            TRAIN.WEIGHTS ./models/model_final.pkl.1 \
+            NUM_GPUS 1
+
+    fi
 
     delete_debug_string $file $line "$debug_str"
 
